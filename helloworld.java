593b666d4e73b8775ec474c9b98b6a494007d9a4
ThomasDordoni <tdordoni@cerebotani.it>
2023-10-28 08:14:12 +0200

    Somma

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
-
 class helloworld{
 	public static void main (String args[]){
+		int n1 = 5, n2 = 3;
 		System.out.println("Ciao Mondo!");
+		System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
 	}
 }
